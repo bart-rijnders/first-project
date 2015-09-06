c9f62bbeb5fe10517ea6c5b0155e1e4d3108829a
bart-rijnders <moro@Moros-MacBook-Pro.local>
2015-09-06 19:22:49 +0200

    removed unneeded objects

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -15,18 +15,5 @@ def index(request):
 		'regen': 2,
 		'Country' : data['country'],
 		'City' : data['city'],
-
 	})
 	return HttpResponse(template.render(context))
-
-
-
-def detail(request, question_id):
-	return HttpResponse("You're looking at question %s." % question_id)
-
-def results(request, question_id):
-	response = "You're looking at the results of question %s."
-	return HttpResponse(response % question_id)
-
-def vote(request, question_id):
-	return HttpResponse("You're voting on question %s." % question_id)
